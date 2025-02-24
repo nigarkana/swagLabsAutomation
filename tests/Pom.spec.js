7fef30821777f6320a6a8c43c35f98ceca3fd006
Nigar Sultana Kana <nigarkana@gmail.com>
2025-02-24 07:58:49 +0000

    Feature branch2 (#6)
    * POM added to tests and pages added
    
    * Login Page added and Test run in the POM.spec.js

--- tests/Pom.spec.js
+++ tests/Pom.spec.js
@@ -1,7 +1,13 @@
 import {test, expect} from '@playwright/test'
+import { LoginPage } from '../pages/loginPage'
+
 
 test ("Handling POM Test", async({page})=>{
     //Login
+    const login = new LoginPage(page);
+    await login.gotoLoginPage();
+    await login.login("pavanol", "test@123")
+    await page.waitForTimeout(3000);
 
     //Home
 
